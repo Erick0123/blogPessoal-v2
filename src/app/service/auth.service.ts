bb5c566876c0cb1b5ea36419a22f617fd9163c1b
Erick0123 <erickalan068@gmail.com>
2021-01-26 11:11:59 -0300

    aplicação de consumo de entrar e cadastrar

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -12,11 +12,11 @@ export class AuthService {
   constructor(
     private http: HttpClient
   ) { }
-  entra(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http//localhost:8080/usuarios/logar', userLogin)
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
   }
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http//localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar',user)
     
   }
 }
