b5d35e14b1344f373faade4ed7b621a8364c1ea9
Erick0123 <erickalan068@gmail.com>
2021-02-01 15:18:34 -0300

    Implementaçao de post/get em postagens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
@@ -12,14 +14,16 @@ import { TemaComponent } from './tema/tema.component';
 const routes: Routes = [
   {path:'', redirectTo:'entrar', pathMatch:'full'},
   
-  {path:'entrar', component:EntrarComponent},
-  {path:'cadastrar', component:CadastrarComponent},
+  {path:'entrar', component: EntrarComponent},
+  {path:'cadastrar', component: CadastrarComponent},
 
-  {path: 'inicio', component:InicioComponent},
+  {path: 'inicio', component: InicioComponent},
   {path: 'tema', component: TemaComponent},
 
   {path: 'tema-edit/:id', component: TemaEditComponent},
-  {path: 'tema-delete/:id', component: TemaDeleteComponent}
+  {path: 'tema-delete/:id', component: TemaDeleteComponent},
+  {path: 'postagem-edit/:id', component: PostagemEditComponent},
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent}
 ];
 
 @NgModule({
